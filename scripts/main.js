c782153f74ce7c24bb4c1f8f7e3dcc82f33e54dd
Enes Terzi <38747547+enestrz@users.noreply.github.com>
2020-11-02 00:52:09 +0300

    Change model of css

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,17 @@ close[0].onclick = function () {
   }, 600);
 };
 //End of Temporary Alert Box
+
+// Back to Top Button
+
+// End of Back to Top Button
+
+//Responsive Navigation Bar
+let navList = document.getElementById("navList");
+let navToggle = document.getElementById("navToggle");
+
+navToggle.addEventListener("click", function() {
+  navList.classList.toggle("active");
+});
+
+//End of Responsive Navigation Bar
